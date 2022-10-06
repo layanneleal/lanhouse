fdc6c985f5161d28d62bee477a36287c8cb6f263
Layanne Bezerra Leal <layanneliz@gmail.com>
2022-10-05 22:28:58 -0300

    Comentei o código

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 function calcularValor () {
+  //Cria referência aos elementos da página
   let inValor = document.getElementById('inValor')
   let inTempo = document.getElementById('inTempo')
   let outTotal = document.getElementById('outTotal')
 
+  //Obtem conteúdos dos campos de entrada
   let valor = Number(inValor.value.replace('.', ',').replace(',','.'))
   let tempo = Number(inTempo.value)
   
-
+  //Calcula o valor a ser pago  e arredonda para cima
   let tempoUso = Math.ceil(tempo / 15)
   let total = tempoUso * valor
 
+  //Altera o conteúdo da linha de resposta
   outTotal.textContent += 'Valor a pagar R$: ' + total.toFixed(2)
 
 }
+//Cria referência ao elemento btCalcularValorPg (botão)
 let btCalcularValorPg = document.getElementById('btCalcularValorPg')
+
+//Registra um evento associado ao botão, para carregar uma função
 btCalcularValorPg.addEventListener('click', calcularValor)
